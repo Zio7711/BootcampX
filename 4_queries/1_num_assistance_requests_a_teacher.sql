51e12a80ebba9ac605fd422a5c487de3881b1f55
Zio <zi.ao.tan@outlook.com>
2021-05-25 19:48:59 +0000

    Get the total number of assistance_requests for a teacher.

---
+++ 4_queries/1_num_assistance_requests_a_teacher.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*) AS total_assistances, teachers.name
+FROM assistance_requests JOIN teachers ON (teachers.id = teacher_id)
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
