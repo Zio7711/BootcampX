ab34e14f72664a1e349e9731cbc9c7ba4bda5659
Zio <zi.ao.tan@outlook.com>
2021-05-25 20:19:52 +0000

    Calculate the average total duration of assistance requests for each cohort.

--- 4_queries/9_cohort_average_assustance_duration.sql
+++ 4_queries/9_cohort_average_assustance_duration.sql
@@ -5,3 +5,5 @@ FROM cohorts JOIN students ON cohorts.id = cohort_id
 JOIN assistance_requests ON students.id = student_id
 GROUP BY cohorts.name
 ORDER BY total_duration) AS total_durations;
+
+-- change commit msg
